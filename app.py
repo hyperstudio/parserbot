2dae9917e7e40cac8712d49a44e8956d00b2205d
Liam Andrew <landrew@mit.edu>
2014-05-02 17:11:46 -0400

    Reformat results returned from Stanford+DBpedia.

--- app.py
+++ app.py
@@ -32,26 +32,33 @@ def run_zemanta():
     results = zemanta.ZemantaAPI().entity_query(payload)
     return jsonify({"results": results})
 
+def get_dbpedia_resources(stanford_results):
+    dbp_results = []
+    for entity_type, entity_list in stanford_results.items():
+        for keyword in entity_list:
+            dbp_response = dbpedia.keyword_search(keyword)['results']
+            dbp_resource = dbp_response[0] if dbp_response else {}
+            response = {
+                'uri': None,
+                'stanford_type': entity_type.lower(),
+                'stanford_name': keyword
+            }
+            response.update(dbp_resource)
+            dbp_results.append(response)
+    return dbp_results
+
 @app.route('/stanford', methods=['GET', 'POST'])
 def run_stanford():
     if request.method == 'GET':
         payload = request.args.get('payload')
     elif request.method == 'POST':
         payload = request.form.get('payload')
-    results = stanford.get_entities(payload)
+    stanford_results = stanford.get_entities(payload)
     # dedupe the lists so that we aren't calling dbpedia repeatedly
-    results = dict((key, list(set(value))) for key, value in results.items())
+    stanford_results = dict((key, list(set(value))) for key, value in stanford_results.items())
     # now get dbpedia URLs for them
-    for entity_type, entity_list in results.items():
-        for index, item in enumerate(entity_list):
-            dbp_resp = dbpedia.keyword_search(item)
-            dbp_results = dbp_resp['results']
-            default_response = {
-                'label': item,
-                'uri': None
-            }
-            results[entity_type][index] = dbp_results[0] if dbp_results else default_response
-    return jsonify(results)
+    dbp_results = get_dbpedia_resources(stanford_results)
+    return jsonify({"results": dbp_results})
 
 @app.route('/dbpedia', methods=['GET', 'POST'])
 def run_dbpedia():
