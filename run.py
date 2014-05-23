08c523c3bd3683e0c4db85105424fc8dd2b339dc
Liam Andrew <landrew@mit.edu>
2014-05-22 22:50:35 -0400

    Change hostname to accept other IPs in production

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import app
 
 app.config.from_object('config')
-app.run(debug=app.config['DEBUG'])
+host = '127.0.0.1' if app.config['DEBUG'] else '0.0.0.0'
+app.run(host=host, debug=app.config['DEBUG'])
