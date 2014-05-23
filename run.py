607b190a5dee11777ce62cfa1dafae93c6de1e4f
Liam Andrew <landrew@mit.edu>
2014-05-22 22:54:44 -0400

    Still debugging Heroku

--- run.py
+++ run.py
@@ -2,4 +2,6 @@ from app import app
 
 app.config.from_object('config')
 host = '127.0.0.1' if app.config['DEBUG'] else '0.0.0.0'
-app.run(host=host, debug=app.config['DEBUG'])
+
+if __name__ == "__main__":
+    app.run(host=host, debug=app.config['DEBUG'])
