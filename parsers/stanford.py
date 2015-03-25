6cd219e4e725d95ccfa59eef62d4212a19165db4
Liam Andrew <liam.p.andrew@gmail.com>
2015-03-25 13:41:44 -0400

    Make Stanford encoding consistent as utf-8

--- parsers/stanford.py
+++ parsers/stanford.py
@@ -5,8 +5,8 @@ import config
 
 def stanford_tag(sentence, classifier=config.STANFORD_DEFAULT_CLASSIFIER, jarfile=config.STANFORD_JARFILE):
     """ Takes any text and optional classifier/jarfile kwargs. Returns raw response from Stanford NER. """
-    t = NERTagger(classifier, jarfile)
-    return t.tag_sents([sentence.encode('ascii', 'ignore').split()])
+    t = NERTagger(classifier, jarfile, encoding='utf-8')
+    return t.tag_sents([sentence.encode('utf-8').split()])
 
 def get_entities(sentence, **kwargs):
     """ Takes any text and optional classifier/jarfile kwargs. Returns a dictionary """
